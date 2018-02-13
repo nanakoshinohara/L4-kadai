85189da02c1b80596c5b67cd159ff9fc27a1410c
篠原七子 <Nanako@shinoharananako-no-MacBook-Air.local>
2018-02-13 22:08:54 +0900

    エラー2つ？

--- SecondKadaiApp/ViewController.swift
+++ SecondKadaiApp/ViewController.swift
@@ -9,7 +9,15 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var label: UIButton!
+    
+    override func prepare(for sugue: UIStoryboardSegue, sender: Any?) {
+        let resultViewController:ResultViewController = segue.destination as! ResultViewController
+        
+        resultViewController.name = ""
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -18,6 +26,9 @@ class ViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func unwind(_ segue: UIStoryboardSegue) {
     }
 
 
